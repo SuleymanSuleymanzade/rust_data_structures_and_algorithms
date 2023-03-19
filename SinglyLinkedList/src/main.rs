c7270c4c5f11c4e8312863c60c11f04a7e040acd
Suleyman Suleymanzade <sul_soft@hotmail.com>
2023-03-19 18:28:35 +0400

    advanced linked list

--- SinglyLinkedList/src/main.rs
+++ SinglyLinkedList/src/main.rs
@@ -1,26 +1,82 @@
 #[derive(Debug)]
-struct linkedlist{
-    head: pointer,
+struct Linklist<T: std::fmt:: Debug + std::marker:: Copy>{
+   head: pointer<T>, 
 }
-
 #[derive(Debug)]
-struct Node{
-    element:i32,
-    next: pointer,
+struct Node<T: std::fmt:: Debug + std::marker:: Copy>{
+    element: T, 
+    next: pointer<T>,     
+  }
+
+type pointer<T> = Option<Box<Node<T>>>; 
+
+impl<T: std::fmt:: Debug + std::marker:: Copy> Linklist<T> {
+   fn create_empty_list() -> Linklist<T> {
+      Linklist { head: None }
+   }
+
+   fn add(&mut self, element: T) {
+  
+   let previous_head = self.head.take(); 
+
+   let new_head = Box::new(Node {
+      element: element, next: previous_head,
+   }); 
+   self.head = Some(new_head); 
+
+   }
+
+
+   fn remove(&mut self) -> Option<T> {
+      let previous_head = self.head.take(); 
+      match previous_head {
+         Some(old_head) => {
+            self.head = old_head.next; 
+            Some(old_head.element)
+         }
+         None => None
+      }
+   }
+
+   fn peek(&self) -> Option<T> {
+      match &self.head {
+         Some(H) => Some(H.element), 
+         None => None
+      }
+   } 
+
+   fn printing(&self) {
+   let mut list_traversal = &self.head; 
+   while true {
+      match list_traversal {
+         Some(Node) => {
+            println!("{:?}", Node.element); 
+            list_traversal = &Node.next;
+         } 
+         None => break, 
+      }
+   }   
+   }
 }
+fn main(){
 
-type pointer = Option<Box<Node>>
+ let mut list = Linklist::create_empty_list(); 
+ list.add(5); 
+ list.add(7); 
+ list.add(10); 
+ list.add(15); 
+ list.add(20); 
 
-fn main() {
-    let node1: Node = Node{element:1, next:None};
-    let node2: Node = Node{element:3, next:Some(Box::new(
-        Node{element:54, next:Some(Box::new(
-            Node{element:54, next: None}
-        ))}
-    ))};
+ println!("The current list is {:?}", list); 
 
-    let list_a = linkedlist(head: Node{element:1, next:None});
-    
 
-println!("{:?}", node2);
+ list.remove(); 
+ println!("The list after call to remove is {:?}", list); 
+
+
+ println!("The peeked element is {:?}", list.peek());
+ 
+
+ list.printing();
+
 }
